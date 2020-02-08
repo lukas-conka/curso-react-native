268eb4ad6c9aa33a9df81865351a9abaf2ad3743
Lucas Santos <lucas.santos@pcservice.com.br>
2020-02-08 02:25:33 -0300

    cal

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/Button'
+import Display from './src/components/Display'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+
+  addDigit = n => {
+    this.setState({ displayValue : n})
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0'})
+  }
+
+  setOperation = operation =>{
+
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}></Display>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory}></Button>
+          <Button label='/' operation onClick={() => this.setOperation('/')}></Button>
+          <Button label='7'></Button>
+          <Button label='8'></Button>
+          <Button label='9'></Button>
+          <Button label='*'></Button>
+          <Button label='4'></Button>
+          <Button label='5'></Button>
+          <Button label='6'></Button>
+          <Button label='-'></Button>
+          <Button label='1'></Button>
+          <Button label='2'></Button>
+          <Button label='3'></Button>
+          <Button label='+'></Button>
+          <Button label='0'></Button>
+          <Button label='.'></Button>
+          <Button label='='></Button>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
 });
